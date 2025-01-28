47680169730074bb8605a9155317fcd5601381bd
Maath JTM Faselli <maathjtmfaselli@gmail.com>
2025-01-28 23:44:41 +0100

    refactor - extract upgradedCharacters

--- js/alzamiento.js
+++ js/alzamiento.js
@@ -52,6 +52,129 @@ const charactersToUpgrade = [
   }
 ];
 
+const upgradedCharacters = [
+  {
+    name: "Vandor Chewbacca",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_chewbacca_vandor.png",
+    relic: "Reliquia 6",
+    assignedTo: "Mik Bat"
+  },
+  {
+    name: "Chirrut Imwe",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_chirrut.png",
+    relic: "Reliquia 7",
+    assignedTo: "Temnyy"
+  },
+  {
+    name: "Mon Mothma",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_monmothma.png",
+    relic: "Reliquia 6",
+    assignedTo: "R3C0"
+  },
+  {
+    name: "Magmatrooper",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_trooperstorm_magma.png",
+    relic: "Reliquia 6",
+    assignedTo: "Blas"
+  },
+  {
+    name: "Sabine Wren",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_sabine_s3.png",
+    relic: "Reliquia 5",
+    assignedTo: "GRANJEDI Tito1"
+  },
+  {
+    name: "BT-1",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_bt1.png",
+    relic: "Reliquia 6",
+    assignedTo: "R3C0"
+  },
+  {
+    name: "Rose Tico",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_rose.png",
+    relic: "Reliquia 7",
+    assignedTo: "Temnyy"
+  },
+  {
+    name: "Jawa Engineer",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_jawa_engineer.png",
+    relic: "Reliquia 5",
+    assignedTo: "Darksneer"
+  },
+  {
+    name: "Logray",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_ewok_logray.png",
+    relic: "Reliquia 5",
+    assignedTo: "GRANJEDI Tito1, Marblagar, Mik Bat"
+  },
+  {
+    name: "Dengar",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_dengar.png",
+    relic: "Reliquia 7",
+    assignedTo: "Maath, Lucas"
+  },
+  {
+    name: "Colonel Starck",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_colonel_stark.png",
+    relic: "Reliquia 6",
+    assignedTo: "Maath"
+  },
+  {
+    name: "Nightsister Acolyte",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_nightsister_acolyte.png",
+    relic: "Reliquia 7",
+    assignedTo: "Cente"
+  },
+  {
+    name: "Hunter",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_bb_hunter.png",
+    relic: "Reliquia 7",
+    assignedTo: "Cente"
+  },
+  {
+    name: "Nightsister Initiate",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_nightsister_initiate.png",
+    relic: "Reliquia 5",
+    assignedTo: "Mik Bat"
+  },
+  {
+    name: "Ugnaut",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_ugnaught.png",
+    relic: "Reliquia 5",
+    assignedTo: "Maath"
+  },
+  {
+    name: "IG-88",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_ig88.png",
+    relic: "Reliquia 7",
+    assignedTo: "Viula"
+  },
+  {
+    name: "Wedge Antilles",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_rebelpilot_wedge.png",
+    relic: "Reliquia 6",
+    assignedTo: "GRANJEDI Tito1"
+  },
+  {
+    name: "Embo",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_embo.png",
+    relic: "Reliquia 6",
+    assignedTo: "Cente"
+  },
+  {
+    name: "Lobot",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_lobot.png",
+    relic: "Reliquia 5",
+    assignedTo: "Marblagar"
+  },
+  {
+    name: "Imperial Probe Droid",
+    image: "https://game-assets.swgoh.gg/textures/tex.charui_probedroid.png",
+    relic: "Reliquia 5",
+    assignedTo: "Mr Zero, Mik Bat"
+  }
+];
+
 function renderCharacterList(characters, listElementId) {
   const listElement = document.getElementById(listElementId);
   listElement.innerHTML = "";
@@ -73,5 +196,5 @@ function renderCharacterList(characters, listElementId) {
 document.addEventListener("DOMContentLoaded", () => {
   renderCharacterList(topPriorityCharacters, "top-characters-to-upgrade-list");
   renderCharacterList(charactersToUpgrade, "characters-to-upgrade-list");
-//  renderCharacterList(upgradedCharacters);
+  renderCharacterList(upgradedCharacters, "upgraded-characters-list");
 });
