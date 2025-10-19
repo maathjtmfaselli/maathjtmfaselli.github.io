bf5b18796a7a698dd3025b5b4c920447289f4369
Maath <maath@maath.com>
2025-10-19 10:59:02 +0200

    header - active link

--- js/dynamic-loader.js
+++ js/dynamic-loader.js
@@ -1,9 +1,22 @@
+function marcarEnlaceActivoCabecera() {
+  const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach(link => {
+    const href = link.getAttribute('href');
+    if (href === currentPage) {
+      link.classList.add('active');
+    }
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     // Cargar la cabecera
     fetch("index-header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("index-dynamic-header").innerHTML = data;
+            marcarEnlaceActivoCabecera();
         });
 
     // Cargar el pie
