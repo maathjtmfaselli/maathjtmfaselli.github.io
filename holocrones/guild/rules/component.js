eda4c1278802c82be65863bf0f5bec6b0a7bbde2
Maath <maath@maath.com>
2026-05-11 09:34:24 +0200

    refactor - holocron - guild rules

---
+++ holocrones/guild/rules/component.js
@@ -0,0 +1,46 @@
+class GuildRulesHolocron extends HTMLElement {
+
+  async connectedCallback() {
+
+    try {
+
+      const response =
+        await fetch(
+          "/holocrones/guild/rules/template.html"
+        );
+
+      if (!response.ok) {
+        throw new Error(
+          "No se pudo cargar el template"
+        );
+      }
+
+      const html =
+        await response.text();
+
+      this.innerHTML = html;
+
+    } catch (error) {
+
+      console.error(
+        "Error cargando holocrón guild-rules",
+        error
+      );
+
+      this.innerHTML = `
+        <section class="holocron-card">
+
+          <p>
+            Error cargando el holocrón.
+          </p>
+
+        </section>
+      `;
+    }
+  }
+}
+
+customElements.define(
+  "holocron-guild-rules",
+  GuildRulesHolocron
+);
